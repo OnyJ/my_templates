cf4fb53be6b86bba58b5d9345412a61c8de849e6
Josselin <julien.josselin75@hotmail.fr>
2022-06-18 12:28:00 +0200

    add react simple boilerplate

---
+++ react_simple_boilerplate/README.md
@@ -0,0 +1,4 @@
+# React Simple Boilerplate
+
+This boilerplate is made to win a few minutes on making a file tree structure, & chosing how files are filled.
+
---
+++ react_simple_boilerplate/src/App.js
@@ -0,0 +1,13 @@
+import "./style/App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Optimax</h1>
+      <p>Some text</p>
+      <a href="#">Some link</a>
+    </div>
+  );
+}
+
+export default App;
